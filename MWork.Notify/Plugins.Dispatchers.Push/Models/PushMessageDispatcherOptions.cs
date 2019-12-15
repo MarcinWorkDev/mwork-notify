1f4cd0a26b0aba081b9bf7e80c31b50b8f2c9ba9
Marcin Li <marcin93li@hotmail.com>
2019-12-15 12:24:11 +0100

    cleanup2

--- MWork.Notify/Plugins.Dispatchers.Push/Models/PushMessageDispatcherOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace MWork.Notify.Plugins.Dispatchers.Push.Models
-{
-    public class PushMessageDispatcherOptions
-    {
-        public string Project { get; set; }
-        
-        public string Credentials { get; set; }
-    }
-}
